8b2f81cd9fca4d948bd22687bce3283c7d6c50da
sidney!! <68286409+Sidnehh@users.noreply.github.com>
2023-11-26 16:32:25 +0100

    Create MostImportantWord.py

---
+++ MostImportantWord.py
@@ -0,0 +1,18 @@
+def MostImportantWord(folder):
+    folder = folder + "/"
+
+    value_to_discard = 0
+
+    files = getTextFilesName(folder)
+
+    max_word = ""
+
+    idf_dict_init = IDF(folder)
+    idf_dict = IDF(folder)
+    for file in files:
+        tf_dict = TF(file, folder)
+        for word in tf_dict.keys():
+            word = str(word)
+            if FinalScoreDict(idf_dict, tf_dict, word) > FinalScoreDict(idf_dict, tf_dict, max_word):
+                max_word = word
+    return max_word
