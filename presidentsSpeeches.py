3fd39b20d8741d7a17ab8ec8f2592126d41fb0b1
sidney!! <68286409+Sidnehh@users.noreply.github.com>
2023-11-26 16:12:01 +0100

    Create presidentsSpeeches.py

---
+++ presidentsSpeeches.py
@@ -0,0 +1,11 @@
+def presidentsSpeeches(presidents, folder):
+    president_speeches = {}
+    for president in presidents:
+        for file in getTextFilesName(folder):
+            if president in file:
+                if(president in president_speeches.keys()):
+                    president_speeches[president].append(file)
+                else:
+                    president_speeches[president] = [file]
+
+    return president_speeches
