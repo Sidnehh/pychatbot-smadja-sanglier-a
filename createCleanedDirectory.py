b9e13c078af7c0eeebb4faa0032b76eb3938068f
sidney!! <68286409+Sidnehh@users.noreply.github.com>
2023-11-26 21:33:28 +0100

    Create createCleanedDirectory.py

---
+++ createCleanedDirectory.py
@@ -0,0 +1,4 @@
+def createCleanedDirectory():                # Crée le dossier "cleaned" et renvoie
+    cleanFolder = "./cleaned/"               # l'emplacement de ce dossier.
+    createFolder(cleanFolder)
+    return cleanFolder
