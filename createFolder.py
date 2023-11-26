7b432e969ea8077849c83094e9e9972639e06d98
sidney!! <68286409+Sidnehh@users.noreply.github.com>
2023-11-26 16:01:46 +0100

    Create createFolder.py

---
+++ createFolder.py
@@ -0,0 +1,6 @@
+def createFolder(folder):  # Crée un dossier dans le répertoire actuel, ayant pour nom l'argument folder.
+    try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+    except OSError:
+        print("Error creating directory : ", folder)
