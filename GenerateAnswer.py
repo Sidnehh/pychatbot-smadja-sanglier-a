1cd111092221b43ad09a0e0cc66e7238168de1e7
Arthur S <arsmadja92@gmail.com>
2023-12-19 09:50:11 +0100

    generate answer never throws an error

--- GenerateAnswer.py
+++ GenerateAnswer.py
@@ -37,18 +37,43 @@ def FirstOccurenceSentence(word, file, folder = "speeches"):
         if word in sentence:
             return sentence
 
+def SentenceWithHighestSimilarity(question, file, folder="speeches"):
+    with open(folder+'/'+file, 'r', encoding = 'utf-8') as f:
+        lines = Lower(''.join(f.readlines()))
+    lines = lines.split('\n')
+    lines = " ".join(lines)
+    lines = lines.split('- ')
+    lines = ''.join(lines)
+    lines = lines.split(' -')
+    lines = ''.join(lines)
+    lines = lines.split('.')
 
+    highest_sentence_id = 0
+    highest_similarity = 0
+    sentence_id = 0
+
+    question_tfidf = SentenceTFIDF(question)
+
+    for sentence in lines:
+        if DictsCosine(SentenceTFIDF(sentence), question_tfidf) > highest_similarity:
+            highest_similarity = DictsCosine(SentenceTFIDF(sentence), question_tfidf)
+            highest_sentence_id = sentence_id
+        sentence_id += 1
+    return lines[highest_sentence_id]
 
 def AnswerGenerator(question, foldr="speeches"):
     if question == "":
         return "Veuillez saisir une question. "
+    id_quest = CleanSentence(question)[0]
     relevantword = MaxTFIDFQuestion(question)
     print("Mot avec le TFIDF le plus élevé :", relevantword)
-    id_quest = CleanSentence(question)[0]
     if relevantword == "":
         return "Pas d'information à ce sujet."
     answer = FirstOccurenceSentence(relevantword, DocumentWithMostSimilarity(question, folder))
+    if answer == None:
+        answer = SentenceWithHighestSimilarity(question, DocumentWithMostSimilarity(question, folder))
     print("Document avec le plus de similarités : ", DocumentWithMostSimilarity(question, folder))
+    print(id_quest, answer)
     if id_quest in question_starters.keys():
         answer = question_starters[id_quest]+answer
     return answer
